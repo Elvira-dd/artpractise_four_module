ec6fe7d952754a0a7427c9a498a12765d6ba4322
Elvira-dd <elvira.6.galieva@yandex.ru>
2023-06-23 10:54:39 +0300

    first commit

---
+++ scripts/pop_up.js
@@ -0,0 +1,13 @@
+// let openButton = document.getElementById("aboutus_button_pop_up");
+// let popup = document.getElementById("about_pop_up_1_i");
+// openButton.addEventListener("click", function () {
+//   // popup.style.display = "block";
+//   console.log("hello");
+// });
+
+let openButton = document.getElementById("button_poster_popup");
+let popup = document.getElementById("about_pop_up_1_i");
+openButton.addEventListener("click", () => {
+  popup.style.display = "block";
+  console.log("hello");
+});
