8d3e10f5e5da1fff78d218b6a93ef9e24660615f
Elvira-dd <elvira.6.galieva@yandex.ru>
2023-06-23 13:44:20 +0300

    draft_all_version_no_font

--- scripts/pop_up.js
+++ scripts/pop_up.js
@@ -4,6 +4,11 @@
 //   // popup.style.display = "block";
 //   console.log("hello");
 // });
+document
+  .getElementsByClassName("services_wind_1")[0]
+  .addEventListener("click", function () {
+    window.location.href = "./close_page.html";
+  });
 
 let openButton = document.getElementById("button_poster_popup");
 let popup = document.getElementById("about_pop_up_1_i");
