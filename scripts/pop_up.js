cacde9beedac04df1ce20522a9b7a1fa8aaa1503
Elvira-dd <elvira.6.galieva@yandex.ru>
2023-06-23 16:22:25 +0300

    near finish

--- scripts/pop_up.js
+++ scripts/pop_up.js
@@ -1,18 +1,68 @@
-// let openButton = document.getElementById("aboutus_button_pop_up");
+// let openButton = document.getElementById("button_poster_popup");
 // let popup = document.getElementById("about_pop_up_1_i");
-// openButton.addEventListener("click", function () {
-//   // popup.style.display = "block";
+// openButton.addEventListener("click", () => {
+//   popup.style.display = "block";
 //   console.log("hello");
 // });
-document
-  .getElementsByClassName("services_wind_1")[0]
-  .addEventListener("click", function () {
-    window.location.href = "./close_page.html";
-  });
+// INDEX>HTML
+function handleResize() {
+  var screenWidth = window.innerWidth;
+  var targetWidth = 600;
 
-let openButton = document.getElementById("button_poster_popup");
-let popup = document.getElementById("about_pop_up_1_i");
-openButton.addEventListener("click", () => {
+  if (screenWidth > targetWidth) {
+    setTimeout(() => {
+      document.getElementById("index_pop_up_1").style.display = "block";
+    }, 1000);
+    setTimeout(() => {
+      document.getElementById("index_pop_up_2").style.display = "block";
+    }, 1500);
+    setTimeout(() => {
+      document.getElementById("index_pop_up_3").style.display = "block";
+    }, 2500);
+    setTimeout(() => {
+      document.getElementById("index_pop_up_4").style.display = "block";
+    }, 1000);
+    setTimeout(() => {
+      document.getElementById("index_pop_up_5").style.display = "block";
+    }, 3000);
+    setTimeout(() => {
+      document.getElementById("index_pop_up_6").style.display = "block";
+    }, 1500);
+    setTimeout(() => {
+      document.getElementById("index_pop_up_7").style.display = "block";
+    }, 3000);
+  }
+}
+
+handleResize();
+
+///
+var divs = document.querySelectorAll(".pop_up");
+
+function changeZIndex() {
+  // Устанавливаем у текущего div z-index равным 100
+  this.style.zIndex = 1000;
+
+  // Проходимся по всем остальным div и устанавливаем z-index равным 1
+  for (var i = 0; i < divs.length; i++) {
+    if (divs[i] !== this) {
+      divs[i].style.zIndex = 1;
+    }
+  }
+}
+
+// Назначаем обработчик клика на каждый div
+for (var i = 0; i < divs.length; i++) {
+  divs[i].addEventListener("click", changeZIndex);
+}
+function PopupAppear(popupID) {
+  let popup = document.getElementById(popupID);
+  let overlay = document.getElementById("overlay");
   popup.style.display = "block";
-  console.log("hello");
-});
+  overlay.style.display = "block";
+}
+function PopupDisappear(popupID) {
+  let popup = document.getElementById(popupID);
+  popup.style.display = "none";
+  overlay.style.display = "none";
+}
